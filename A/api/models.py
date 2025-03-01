7d625e4a86059dbe6123a87219193dfce9b503ae
Erfan-aminian <aminian158@gmail.com>
2025-03-01 14:31:24 -0500

    Serializers

---
+++ A/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    bio = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
