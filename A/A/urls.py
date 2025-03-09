5203f88c9a12197af85b0b44e317cf8f7dbdd042
Erfan-aminian <aminian158@gmail.com>
2025-03-09 10:45:49 -0400

    update and delete function

--- A/A/urls.py
+++ A/A/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from api.views import user_profile
+from api.views import user_profile, update_user_profile, delete_user_profile
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', user_profile, name='user_profile'),
+    path('user/<id>/', update_user_profile, name='update_user_profile'),
+    path('user/del/<id>/', delete_user_profile, name='delete_user_profile'),
 ]
--- A/api/views.py
+++ A/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.response import Response,
+from rest_framework.response import Response
 from .serializers import UserProfileSerializer, CreateUserSerializer
 from .models import UserProfile
-
+from rest_framework import status
 # Create your views here.
 @api_view(['GET', 'POST'])
 def user_profile(request):
@@ -19,6 +19,23 @@ def user_profile(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+def update_user_profile(request, id):
+    user_profile_objects = UserProfile.objects.get(id=id)
+    serializer = UserProfileSerializer(instance=user_profile_objects, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def delete_user_profile(request, id):
+    user_profile_objects = UserProfile.objects.get(id=id)
+    user_profile_objects.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 
 
